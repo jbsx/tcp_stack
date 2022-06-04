371016828bedf5bb9ea83f4e1e08d768f54556d3
Jaish Singh <jaishsingh@hotmail.com>
2022-06-04 10:56:03 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,14 @@ fn main() -> io::Result<()>{
     let mut buff = [0u8; 1504];
     loop{
         let recv_len = nic.recv(&mut buff).unwrap();
-        let flags = u16::from_be_bytes([buff[0], buff[1]]);
-        let protocol = u16::from_be_bytes([buff[2], buff[3]]);
-        eprintln!("recieved {} bytes of protocol {:x?} with flags: {:x?}: {:x?}", recv_len, protocol, flags, &buff[4..recv_len-4]);
+        if u16::from_be_bytes([buff[2], buff[3]]) != 0x0800 {continue}
+        match etherparse::Ipv4Header::from_slice(&buff[4..recv_len]){
+            Ok((header, _))=>{
+                eprintln!("Recieved {} bytes of IPV4 packet from {:?}", header.total_len(), header.source)
+            },
+            Err(e)=>{
+                eprintln!("{}", e)
+            }
+        }
     }
 }
