4fba8db70bbca89e7b7dd7197d07d3cd8cc052f5
Jaish Singh <jaishsingh@hotmail.com>
2022-06-11 18:27:05 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -22,24 +22,23 @@ fn main() -> io::Result<()> {
             continue;
         }
         match etherparse::Ipv4HeaderSlice::from_slice(&buff[4..recv_len]) {
-            Ok(header) => {
+            Ok(ip_header) => {
                 eprintln!(
                     "Recieved {} bytes of IPV4 packet from {:?} -> {:?}",
-                    header.payload_len(),
-                    header.source_addr(),
-                    header.destination_addr()
+                    ip_header.payload_len(),
+                    ip_header.source_addr(),
+                    ip_header.destination_addr()
                 );
-                match etherparse::TcpHeaderSlice::from_slice(&buff[4 + header.slice().len()..]) {
-                    Ok(res) => {
-                        eprintln!("{:?}", &res);
-                        connections
-                            .entry(AddrQuad {
-                                src: header.source_addr(),
-                                src_port: res.source_port(),
-                                dst: header.destination_addr(),
-                                dst_port: res.destination_port(),
-                            })
-                            .or_default();
+                match etherparse::TcpHeaderSlice::from_slice(&buff[4 + ip_header.slice().len()..]) {
+                    Ok(tcp_header) => {
+                        let data_len = ip_header.slice().len() + tcp_header.slice().len() + 4;
+                        connections.entry(AddrQuad {
+                                src: ip_header.source_addr(),
+                                src_port: tcp_header.source_port(),
+                                dst: ip_header.destination_addr(),
+                                dst_port: tcp_header.destination_port(),
+                            }).or_default()
+                            .on_packet(tcp_header, ip_header, &buff[data_len..]);
                     }
                     Err(e) => {
                         eprintln!("{}", e);
--- src/tcp.rs
+++ src/tcp.rs
@@ -5,7 +5,18 @@ pub(crate) struct State{
 impl Default for State{
     fn default() -> Self {
         State { 
-
+            
         }
+    }
+}
+
+impl State{
+    pub fn on_packet<'a>(
+        &mut self,
+        tcp_header: etherparse::TcpHeaderSlice<'a>,
+        ip_header: etherparse::Ipv4HeaderSlice<'a>,
+        buff: &'a [u8]
+    ){
+        eprintln!("{:?} , {:?}, {:?}", tcp_header, ip_header, buff);
     }
 }
